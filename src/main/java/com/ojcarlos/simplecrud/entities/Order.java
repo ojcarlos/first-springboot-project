9e72a527cea3647a5bc37f5942613141e2b91108
ojcarlos <carlos.ojunior00@gmail.com>
2023-03-06 12:26:34 -0400

    subtotal & total methods

--- src/main/java/com/ojcarlos/simplecrud/entities/Order.java
+++ src/main/java/com/ojcarlos/simplecrud/entities/Order.java
@@ -89,6 +89,14 @@ public class Order implements Serializable {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+    public Double getTotal(){
+        double sum = 0.0;
+        for (OrderItem x:
+             items) {
+            sum = sum + x.getSubTotal();
+        }
+        return sum;
+    }
 
     @Override
     public boolean equals(Object o) {
